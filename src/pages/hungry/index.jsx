75da148648744259707c32f2d1994f463fbf1e98
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-06-04 16:55:52 -0300

    start creating hungry page

--- src/pages/hungry/index.jsx
+++ src/pages/hungry/index.jsx
@@ -1,11 +1,57 @@
 import { Footer } from '../../components/footer';
 import { Header } from '../../components/header';
+import { Title } from '../../components/title';
+import img1 from '../../assets/hungry-1.png';
 import * as S from './styles';
+import { Banner } from '../../components/banner';
+import { Divider } from '../../components/divider';
+import { Text } from '../../components/text';
 
 export function HungryPage() {
   return (
     <S.Wrapper>
       <Header />
+      <S.Image1 src={img1} alt="" />
+      <Title size="25rem" uppercase>Fome</Title>
+      <Divider height="80px" />
+      <Banner variant>The Radicle community exists in various places. Join us!</Banner>
+      <Divider height="32rem" />
+      <S.TextWrapper>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae ex diam.
+          Quisque et ipsum eros. Nunc aliquam elit non felis bibendum, cursus feugiat erat suscipit.
+          Donec porttitor nunc dignissim eros egestas aliquam in eu erat. Praesent odio elit, auctor
+          id justo non, condimentum finibus nisi.
+        </Text>
+      </S.TextWrapper>
+      <Divider height="80px" />
+      <Title size="10.5rem">
+        Utilizando a Tecnologia para
+        <br />
+        melhorar a agricultura
+      </Title>
+      <Divider height="80px" />
+      <S.HorizontalSection>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae ex diam.
+          Quisque et ipsum eros. Nunc aliquam elit non felis bibendum, cursus feugiat erat suscipit.
+          Donec porttitor nunc dignissim eros egestas aliquam in eu erat. Praesent odio elit, auctor
+          id justo non, condimentum finibus nisi.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae ex diam.
+          Quisque et ipsum eros. Nunc aliquam elit non felis bibendum, cursus feugiat erat suscipit.
+          Donec porttitor nunc dignissim eros egestas aliquam in eu erat. Praesent odio elit, auctor
+          id justo non, condimentum finibus nisi.
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae ex diam.
+          Quisque et ipsum eros. Nunc aliquam elit non felis bibendum, cursus feugiat erat suscipit.
+          Donec porttitor nunc dignissim eros egestas aliquam in eu erat. Praesent odio elit, auctor
+          id justo non, condimentum finibus nisi.
+        </Text>
+      </S.HorizontalSection>
+      <Divider height="80px" />
       <Footer />
     </S.Wrapper>
   );
--- src/pages/hungry/styles.js
+++ src/pages/hungry/styles.js
@@ -5,3 +5,28 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+
+export const Image1 = styled.img`
+  width: 100%;
+  max-height: 60rem;
+  object-fit: cover;
+  padding: 8rem;
+  position: absolute;
+  top: 20rem;
+`;
+
+export const TextWrapper = styled.div`
+  max-width: 60rem;
+
+  & > * {
+    text-align: center;
+  }
+`;
+
+export const HorizontalSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 4.8rem;
+  padding: 6rem 12rem;
+`;
