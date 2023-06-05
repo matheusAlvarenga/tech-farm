1dae609a868e08b9e116ec32bf75e51a3bcd6a73
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2023-06-05 12:57:55 -0300

    finish our solution base page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,7 @@ import { Home } from '../pages/home';
 import { HungryPage } from '../pages/hungry';
 import { InsecurityPage } from '../pages/insecurity';
 import { FarmingPage } from '../pages/farming';
+import { OurSolutionPage } from '../pages/our-solution';
 
 export function AppRouter() {
   return (
@@ -12,6 +13,7 @@ export function AppRouter() {
         <Route path="/fome" exact element={<HungryPage />} />
         <Route path="/inseguranca" exact element={<InsecurityPage />} />
         <Route path="/agricultura" exact element={<FarmingPage />} />
+        <Route path="/nossa-solucao" exact element={<OurSolutionPage />} />
         <Route path="*" element={<h1>Erro</h1>} />
       </Routes>
     </BrowserRouter>
